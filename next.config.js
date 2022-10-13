c872291f5c5a1eda9ea56c6dea56aa750bf00660
ahya37 <ahmadyaniid37@gmail.com>
2022-10-13 22:26:56 +0700

    add package

--- next.config.js
+++ next.config.js
@@ -2,19 +2,11 @@ const path = require("path");
 
 const withSass = require("@zeit/next-sass");
 
-const withOptimizedImages = require('next-optimized-images');
-
 module.exports = withSass({
   /* bydefault config  option Read For More Optios
 here https://github.com/vercel/next-plugins/tree/master/packages/next-sass
 */
   cssModules: true,
-});
-
-module.exports = withOptimizedImages({
-  /* config for next-optimized-images */
-
-  // your config for other plugins or the general next.js here...
 });
 
 module.exports = {
